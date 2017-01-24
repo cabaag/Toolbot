999744f35b5409b01a6ac055acefe20a753de93e
Carlos Barranco <caba9313@gmail.com>
2017-01-23 18:33:11 -0600

    Preparing for upgrade to angular-cli 1.0.0-beta.26

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CookieService } from 'angular2-cookie/services/cookies.service';
 import { Project } from './../../services/project';
 import { ProjectsService } from './../../services/projects.service';
 
@@ -12,15 +13,25 @@ export class ProjectsComponent implements OnInit {
   listView: boolean = true;
   projects: Project[];
   constructor(
-    private _projects: ProjectsService
+    private _projects: ProjectsService,
+    private _cookieService:CookieService
   ) {
     this.projects = _projects.getProjects();
   }
 
   ngOnInit() {
+    this.listView = this.getViewPreferences();
   }
 
   changeView(view: string): void {
     this.listView = view === 'list';
+    this._cookieService.put('typeViewProjects', this.listView + '');
+  }
+
+  /*
+  *  Get prefererences of the view of projects
+  */
+  getViewPreferences(): boolean {
+    return this._cookieService.get('typeViewProjects') === 'true';
   }
 }
